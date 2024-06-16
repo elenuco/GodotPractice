e13e16b5b494b46d4a1ffeb24137ad0dbf97435d
Evelyn Elena Campos <elenuco36@gmail.com>
2024-06-16 10:30:02 -0600

    Feat: New scropshas been added and new player function has suefered modifications

--- Escenas/personajeJugador2D.gd
+++ Escenas/personajeJugador2D.gd
@@ -1,15 +1,19 @@
 extends KinematicBody2D
 #Define the character speed
-var speed= 500
+export var speed= 500
 #	Dfining Direcction of the player
 var direction = Vector2()
 #levantadno una funcion
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_right"):
-		direction.x = -speed
-	elif Input.is_action_pressed("ui_left"):
 		direction.x = +speed
+		$AnimatedSprite.flip_h = false
+		$AnimatedSprite.play("ranitaCorre")
+	elif Input.is_action_pressed("ui_left"):
+		direction.x = -speed
+		$AnimatedSprite.flip_h = true
+		$AnimatedSprite.play("ranitaCorre")
 	else:
 		direction.x = 0
-		
+		$AnimatedSprite.play("ranitaEspera")
 	move_and_slide(direction, Vector2.UP)
